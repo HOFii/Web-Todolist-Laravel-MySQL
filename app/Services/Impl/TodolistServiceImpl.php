c4dadeef748c768f1fdbea8a895c3bf952b5e0b1
Gusti Alifiraqsha <alifiraqshaakbar@gmail.com>
2024-04-30 16:34:17 +0700

    update app/Services

--- app/Services/Impl/TodolistServiceImpl.php
+++ app/Services/Impl/TodolistServiceImpl.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Impl;
 
+use App\Models\Todo;
 use App\Services\TodolistService;
 use Illuminate\Support\Facades\Session;
 
@@ -9,32 +10,41 @@ class TodolistServiceImpl implements TodolistService
 {
     public function saveTodo(string $id, string $todo): void
     {
-        if (!Session::exists("todolist")) {
-            Session::put("todolist", []);
-        }
+        // if (!Session::exists("todolist")) {
+        //     Session::put("todolist", []);
+        // }
 
-        Session::push("todolist", [
+        // Session::push("todolist", [
+        //     "id" => $id,
+        //     "todo" => $todo
+        // ]);
+        $todo = new Todo([
             "id" => $id,
             "todo" => $todo
         ]);
+        $todo->save();
     }
     public function getTodolist(): array
     {
-        return Session::get("todolist", []);
+        return Todo::query()->get()->toArray();
     }
 
     public function removeTodo(string $todoId)
     {
-        $todolist = Session::get("todolist");
+        //     $todolist = Session::get("todolist");
 
-        foreach ($todolist as $index => $value) {
-            if ($value['id'] == $todoId) {
-                unset($todolist[$index]);
-                break;
-            }
+        //     foreach ($todolist as $index => $value) {
+        //         if ($value['id'] == $todoId) {
+        //             unset($todolist[$index]);
+        //             break;
+        //         }
+        //     }
+
+        //     Session::put("todolist", $todolist);
+        // }
+        $todo = Todo::query()->find($todoId);
+        if ($todo != null) {
+            $todo->delete();
         }
-
-        Session::put("todolist", $todolist);
     }
-
 }
